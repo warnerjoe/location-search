f42ce52652890c3deae60598edccac5cd0a6028c
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-09-23 22:40:57 -0500

    Added display for search results

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,19 +1,21 @@
 import type { Place } from '../api/Place';
-import { useState } from 'react';
+import { useState, Fragment} from 'react';
 import { search } from '../api/search';
 
 interface LocationSearchProps {
     onPlaceClick: (place: Place) => void;
 }
 
-export default function LocationSearch( onPlaceClick: LocationSearchProps ) {
+export default function LocationSearch( { onPlaceClick }: LocationSearchProps ) {
     const [term, setTerm] = useState('');
     const [places, setPlaces] = useState< Place[] >([]);
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        await search(term);
+        const results = await search(term);
+
+        setPlaces(results);
     }
 
     return (
@@ -24,6 +26,20 @@ export default function LocationSearch( onPlaceClick: LocationSearchProps ) {
                 </label>
                 <input className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full" id="term" value={term} onChange={e => setTerm(e.target.value)}/>
             </form>
+            <h1 className="font-bold mt-6">Found locations</h1>
+            <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
+                {
+                    places.map(place => {
+                        return (
+                            <Fragment key={place.id}>
+                                <p className="text-sm">{place.name}</p>
+                                <button className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded" onClick={() => onPlaceClick(place)}>Go</button>
+                                <div className="border-b w-full col-span-2" />
+                            </Fragment>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
