4a4b9f04152600892c6f86aaaa0c119c0ebdcfa2
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-09-23 22:33:29 -0500

    Connected to openstreetmap api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import type { Place } from './api/Place';
+import { useState } from 'react';
 import Map from './components/Map';
 import LocationSearch from './components/LocationSearch';
 
 function App() {
+  const [place, setPlace] = useState<Place | null>(null);
+
   return (
     <>
       <div className="h-screen w-screen grid grid-cols-12">
         <div className="col-span-3 p-2">
-          <LocationSearch />    
+          <LocationSearch onPlaceClick={(p) => setPlace(p)}/>    
         </div>
         <div className="col-span-9">
-          <Map />
+          <Map place={ place }/>
         </div>
         
       </div>
--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,9 +1,29 @@
 import type { Place } from '../api/Place';
+import { useState } from 'react';
+import { search } from '../api/search';
 
-export default function LocationSearch() {
+interface LocationSearchProps {
+    onPlaceClick: (place: Place) => void;
+}
+
+export default function LocationSearch( onPlaceClick: LocationSearchProps ) {
+    const [term, setTerm] = useState('');
+    const [places, setPlaces] = useState< Place[] >([]);
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        await search(term);
+    }
+
     return (
         <div>
-            Search for a location.
+            <form onSubmit={handleSubmit}>
+                <label className="font-bold" htmlFor="term">
+                    Search
+                </label>
+                <input className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full" id="term" value={term} onChange={e => setTerm(e.target.value)}/>
+            </form>
         </div>
     )
 }
